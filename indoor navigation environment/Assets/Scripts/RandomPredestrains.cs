69bd6f99b789396c65c9f7f96bf21affde152f4b
Ke <38123499+kekekekekekekekeke@users.noreply.github.com>
2019-07-05 11:47:25 -0700

    traffic and reset toggle

---
+++ indoor navigation environment/Assets/Scripts/RandomPredestrains.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPredestrains : MonoBehaviour {
+
+    private Vector3 spawnLocation;
+    public GameObject[] m_Predestrains;
+    public float density;
+    public float traffic_Max = 10;
+    private float traffic = 0;
+    // Use this for initialization
+    void Start()
+    {
+        spawnLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % density == 0 && traffic < traffic_Max)
+        {
+            CreatePredestrains();
+            traffic += 1;
+        }
+    }
+
+    void CreatePredestrains()
+    {
+       // spawnLocation += new Vector3(Random.value *10, 0, Random.value*5);
+        Instantiate(m_Predestrains[Random.Range(0,m_Predestrains.Length)], spawnLocation, Quaternion.identity);
+    }
+}
---
+++ indoor navigation environment/Assets/Scripts/ToggleReset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ToggleReset : MonoBehaviour {
+
+    // Use this for initialization
+    Toggle myToggle;
+    public Toggle[] allToggles;
+    public GameObject car;
+
+    void Start()
+    {
+        myToggle = GetComponent<Toggle>();
+        myToggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(myToggle);
+        });
+    }
+
+
+    void ToggleValueChanged(Toggle change)
+    {
+        for (int i = 0; i < allToggles.Length; ++i)
+        {
+            allToggles[i].isOn = false;
+        }
+        myToggle.isOn = false;
+        car.transform.position = new Vector3(-96.8f, -2.793f, 8.0f);
+        car.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+}
