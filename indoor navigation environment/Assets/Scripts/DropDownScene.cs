ab2dba1615e781f6cb2f5831fa87dbb6305aaaef
Ke <38123499+kekekekekekekekeke@users.noreply.github.com>
2019-07-10 14:29:08 -0700

    working main menu

---
+++ indoor navigation environment/Assets/Scripts/DropDownScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DropDownScene : MonoBehaviour {
+    List<string> scenes = new List<string>() { "Please select Scene", "Warehouse", "Haunted House", "School Corridor" };
+    List<string> modes = new List<string>() { "Please select Mode", "VR mode", "Display mode" };
+    public Dropdown sceneDropdown;
+    public Dropdown modeDropDown;
+    public Button start;
+    private int selectScene;
+    private int selectMode;
+    public void SceneDropdown_IndexChanged(int index)
+    {
+        selectScene = index;
+        if(index == 0)
+        {
+            ColorBlock cb = sceneDropdown.colors;
+            cb.normalColor = Color.black;
+            sceneDropdown.colors = cb;
+        }
+    }
+    public void ModeDropdown_IndexChanged(int index)
+    {
+        selectMode = index;
+        if (index == 0)
+        {
+            ColorBlock cb = sceneDropdown.colors;
+            cb.normalColor = Color.black;
+            sceneDropdown.colors = cb;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        PopulateList();
+	}
+	
+	// Update is called once per frame
+	void PopulateList () {
+        sceneDropdown.AddOptions(scenes);
+        modeDropDown.AddOptions(modes);
+	}
+
+    public void ButtonClicked()
+    {
+        if (selectMode != 0 && selectScene != 0)
+        {
+            Debug.Log(selectScene);
+            SceneManager.LoadScene(selectScene);
+        }
+    }
+}
