7f5e25fb16c7a2ea944e1a09b5099b9442f0202d
Ke <38123499+kekekekekekekekeke@users.noreply.github.com>
2019-07-18 14:43:27 -0700

    change toggle to button

---
+++ indoor navigation environment/Assets/Scripts/SystemController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SystemController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Toggle[] allToggles;
+    public GameObject car;
+    private Vector3 oldPosition;
+    public GameObject Traffics;
+    public GameObject SideCameras;
+    public GameObject GroundTruth;
+    public GameObject Depth;
+    public GameObject Semantics;
+    public void ResetClicked()
+    {
+        for (int i = 0; i < allToggles.Length; ++i)
+        {
+            allToggles[i].isOn = false;
+        }
+        car.transform.position = oldPosition;
+        car.transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+    private void Start()
+    {
+        oldPosition = car.transform.position;
+        Traffics.SetActive(false);
+        SideCameras.SetActive(false);
+        GroundTruth.SetActive(false);
+        Depth.SetActive(false);
+        Semantics.SetActive(false);
+    }
+
+    public void GoBackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void EnableTraffics()
+    {
+        Traffics.SetActive(!Traffics.activeSelf);
+    }
+    public void EnableSideCameras()
+    {
+        SideCameras.SetActive(!SideCameras.activeSelf);
+    }
+    public void EnableGroundTruth()
+    {
+        GroundTruth.SetActive(!GroundTruth.activeSelf);
+    }
+    public void EnableDepth()
+    {
+        Depth.SetActive(!Depth.activeSelf);
+    }
+    public void EnableSemantics()
+    {
+        Semantics.SetActive(!Semantics.activeSelf);
+    }
+}
---
+++ indoor navigation environment/Assets/Scripts/ToggleExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToggleExit : MonoBehaviour
+{
+    Toggle myToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myToggle = GetComponent<Toggle>();
+        myToggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(myToggle);
+        });
+
+    }
+
+    void ToggleValueChanged(Toggle change)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
